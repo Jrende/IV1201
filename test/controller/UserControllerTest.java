14d4bb8a81d6b1f403e819dc87aeab28a5778d6f
Jenny Olsson <rymdsnigel@gmail.com>
2013-02-05 13:13:21 +0100

    testing for User

---
+++ test/controller/UserControllerTest.java
@@ -0,0 +1,26 @@
+package controller;
+
+import static org.junit.Assert.*;
+import static org.fest.assertions.Assertions.assertThat;
+import play.libs.WS;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.running;
+import static play.test.Helpers.testServer;
+
+import org.junit.Test;
+
+public class UserControllerTest {
+
+    @Test
+    public void testIndexWithTestServerRunnable() {
+        running(testServer(3333), new Runnable() {
+            @Override
+            public void run() {
+                assertThat(
+                        WS.url("http://localhost:3333").get().get().getStatus()
+                ).isEqualTo(OK);
+            }
+        });
+    }
+
+}
---
+++ test/model/UserTest.java
@@ -0,0 +1,32 @@
+package model;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.*;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.running;
+
+import models.User;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void findUser() {
+		running(fakeApplication(), new Runnable() {
+			public void run() {
+				assertThat(User.findByUsername("snigel").username).isEqualTo("snigel");
+			}
+		});
+	}
+	
+	@Test
+	public void badUser() {
+		running(fakeApplication(), new Runnable() {
+			public void run() {
+				assertThat(User.findByUsername("snigel").username).isNotEqualTo("fisk");
+			}
+		});
+	}
+
+}
