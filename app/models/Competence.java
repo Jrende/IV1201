bbbfbb64e019566a08f70536b6073ceae0708c9f
Johan Rende <johan@rende.se>
2013-02-11 13:53:03 +0100

    Added competence model.

---
+++ app/models/Competence.java
@@ -0,0 +1,25 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import play.data.format.Formats;
+import play.data.validation.Constraints;
+import play.db.ebean.Model;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+@Entity
+@Table(name = "competence")
+public class Competence extends Model {
+	@Id
+	int person_id;
+	@Constraints.Required(message="Required")
+	String name;
+}
